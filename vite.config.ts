231414cd4a62ad6f21aa18253b439a08bd1122ef
殊怀丶 <lvzhihe_123@qq.com>
2026-02-24 19:52:34 +0800

    fex: 更改 API 地址

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import tailwindcss from '@tailwindcss/vite';
 
@@ -12,7 +12,7 @@ export default defineConfig({
             proxyTargets.map((name) => [
                 `/${name}`,
                 {
-                    target: 'http://localhost:25001',
+                    target: 'http://lvshuhuai.cn:25001',
                     changeOrigin: true,
                 },
             ]),
